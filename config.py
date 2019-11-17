bd963ec396c8344b006fb2ded41d5d17137145ef
xwp <xwp_fullstack@163.com>
2019-11-17 16:39:44 +0800

    工厂类方法创建应用实例

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import redis
+# from config import Config
 class Config(object):
     """工程配置信息"""
     SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
@@ -14,3 +15,15 @@ class Config(object):
     SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
     PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
 
+class DevelopementConfig(Config):
+    """开发模式下的配置"""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """生产模式下的配置"""
+    DEBUG = True
+# 定义配置字典
+config = {
+    "development": DevelopementConfig,
+    "production": ProductionConfig
+}
